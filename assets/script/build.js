6aff9d4ba605bb26b524ac4b7704a11c86d5bb70
Benjamin <nj.benliu@gmail.com>
2017-06-17 12:33:30 -0400

    Update build.js

--- assets/script/build.js
+++ assets/script/build.js
@@ -1,4 +1,3 @@
 document.getElementById("header").innerHTML = '<a href="nj-benliu.github.io" id="home"></a>';
 document.getElementById("home").innerHTML = 'Benjamin';
-document.getElementById("footer").innerHTML = '© Benjamin Liu 2016. Do not distribute!';
 
